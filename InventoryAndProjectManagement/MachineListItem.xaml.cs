2a451a75390c3995cd5c845cfd9b16094fe28cb4
Dylan Burr <djbn65@umsystem.edu>
2020-10-19 00:34:26 -0500

    Add MachineListItem control
    This change adds the machinelist item control which is
    a card that shows relevant information with some
    useful buttons at the bottom. Eventually, clicking the
    card should result in it flipping over and revealing more
    info as a popup. It will appear that this info is on the
    "back" of the card.

---
+++ InventoryAndProjectManagement/MachineListItem.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InventoryAndProjectManagement
+{
+    /// <summary>
+    /// Interaction logic for MachineListItem.xaml
+    /// </summary>
+    public partial class MachineListItem : UserControl, INotifyPropertyChanged
+    {
+        private string _title = null;
+        public string Title { get => _title; set { _title = value; NotifyPropertyChanged(); } }
+
+        private string _path = null;
+        public string ImgPath { get => _path; set { _path = value; NotifyPropertyChanged(); } }
+
+        public MachineListItem()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ InventoryAndProjectManagement/TitleToAbbrConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InventoryAndProjectManagement
+{
+    internal class TitleToAbbrConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string abbreviation = "";
+
+            foreach (string word in ((string)value).Split(' '))
+            {
+                abbreviation += char.ToUpper(word[0]);
+            }
+
+            return abbreviation;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
